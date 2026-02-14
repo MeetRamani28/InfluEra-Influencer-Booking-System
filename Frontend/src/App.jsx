55bf0c3fe4347124463d59caf7c15eb9c20fab0a
Meet Ramani <meetramani21@gmail.com>
2026-02-14 18:53:02 +0530

    Intital Setup For InfluEra Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen text-2xl flex items-center justify-center'>
+      Welcome To InfluEra Frontend!...✨
+    </div>
+  )
+}
+
+export default App
